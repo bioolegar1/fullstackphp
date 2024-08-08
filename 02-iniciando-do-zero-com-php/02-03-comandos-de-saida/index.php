4bbaf79d7d237852ff0b34cd1ecfd7cdeaf30286
bioolegar1 <bioolegari@gmail.com>
2024-08-08 19:33:00 -0300

    validador de cpf adicionado

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,39 +7,11 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
-echo " <p> Olá Mundo! " , " ", "<span class='tag'>#BoraProgramar!</span>" , " </p>";
-
-$hello = "Olá Mundo!";
-$code = "<span class='tag'>#BoraProgramar!</span>";
-
-echo "<p>$hello</p>";
-echo '<p>$hello</p>';
-
-$day = "dia";
-echo "<p> Falta 1 $day para o evento! </p>";
-echo "<p> Falta 2 {$day}s para o evento! </p>";
-
-echo "<h3>{$hello}</h3>";
-echo "<h4>{$hello} {$code}</h4>";
-
-echo '<h3>' . $hello . " " . $code . '</h3>';
-?>
-
-    <H4> <?= $hello ?> <?= $code ?> </h4>
-
-<?php
-
 
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
-
-//print $hello, $code;
-print $hello;
-print $code;
-
-print "<h3>{$hello} {$code}</h3>";
 
 
 /**
@@ -47,33 +19,11 @@ print "<h3>{$hello} {$code}</h3>";
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
-$array = [
-"company" => "BioOlegari",
-    "course" => "PHPFullStack",
-    "class" => "Comandos de Saída"
-];
-
-//print $array;
-print_r($array);
-echo"<pre>", print_r($array, true), "</pre>";
-
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
-
-$article = "<article><h1>%s<p>%s</p></h1></article>";
-$title = "{$hello} {$code}";
-$subtitle   ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
-    it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-    typesetting,remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-    sheets containing.Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-    including versions of Lorem Ipsum.";
-
-printf($article, $title, $subtitle);
-echo sprintf($article, $title, $subtitle);
 
 
 /**
@@ -81,18 +31,8 @@ echo sprintf($article, $title, $subtitle);
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
-$company = "<article><h1>Escola %s<p>Curso<b>, aula <b>%s</p></h1></article>";
-vprintf($company, $array);
-echo vsprintf($company, $array);
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
-
-var_dump(
-    $array,
-    $hello,
-    $company
-);
-
--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,33 +9,6 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
-$teste = true;
-if($teste) {
-    var_dump(true);
-}else {
-    var_dump(false);
-}
-
-$age = 52;
-if($age < 32) {
-    var_dump("Bandas Colorias");
-}elseif($age > 20 && $age < 50){
-var_dump("òtimas Bandas");
-} else {
-    var_dump("Rock and Roll Raiz");
-}
-
-$hora = 3;
-
-if($hora >= 5 && $hora < 23 ) {
-    if ($hora < 7) {
-        var_dump("Bob Marley");
-    } else {
-        var_dump("After Bridge");
-    }
-}else {
-    var_dump("zzZZzZZzZZzzZZ");
-}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
@@ -43,56 +16,12 @@ if($hora >= 5 && $hora < 23 ) {
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
-$rock = "";
-if(isset($rock)) {
-    var_dump("rock And Roll");
-}
-
-//negação:
-$rocky = "";
-if(!isset($rocky)) {
-    var_dump("rock And Roll");
-}else {
-    var_dump("Die!");
-}
-
-$rockAndRoll = "AC/DC";
-if(!empty($rockAndRoll)) {
-    var_dump("Rock existe e toca {$rockAndRoll}");
-}else{
-    var_dump("Não existe ou não está tocando!");
-}
-
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
  */
 fullStackPHPClassSession("switch", __LINE__);
 
-$payment = "credit card";
-switch ($payment) {
-    case "billet_printed":
-var_dump("Boleto Impresso");
-        break;
-    case'canceled':
-var_dump("Pagamento Cancelado");
-        break;
-    case 'past_due':
-    case 'pending':
-var_dump("Aguardando Pagamento");
-
-        break;
-    case 'approved':
-    case 'completed':
-var_dump("Pagamento Aprovado");
-        break;
-    default:
-        var_dump("Erro ao processar pagamento!");
-        break;
-};
-
-
-//teste
 
 
 
